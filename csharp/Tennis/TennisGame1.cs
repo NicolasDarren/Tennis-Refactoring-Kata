ba18cd6342d00ec242a6e9e9c05fec873d86337e
Soriya Das <soriya.das@chillisoft.co.za>
2021-02-26 15:29:01 +0200

    More TennisGame1 refactoring

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -4,79 +4,57 @@ namespace Tennis
 {
     internal class TennisGame1 : ITennisGame
     {
-        private string player1Name;
-        private int player1Points;
-        private string player2Name;
-        private int player2Points;
+        public string Player1Name { get; }
+        public string Player2Name { get; }
+        private int _player1Points;
+        private int _player2Points;
 
         public TennisGame1(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            Player1Name = player1Name;
+            Player2Name = player2Name;
         }
 
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
-                player1Points += 1;
-            else
-                player2Points += 1;
+          if (playerName == "player1")
+            _player1Points += 1;
+          else
+            _player2Points += 1;
         }
 
         public string GetScore()
         {
-            var score = "";
-            if (player1Points == player2Points)
+            if (PointsAreEqual())
             {
-                return GetScoreWhenEqualPoints();
+                return GetScoreWhenPointsAreEqual();
             }
 
-            if (player1Points >= 4 || player2Points >= 4)
+            if (_player1Points >= 4 || _player2Points >= 4)
             {
-                var pointGap = player1Points - player2Points;
+                var pointGap = _player1Points - _player2Points;
                 return PointGapIsMoreThanOne(pointGap)
                     ? GetWinningScore(pointGap)
                     : GetAdvantageScore(pointGap);
             }
 
-            for (var i = 1; i < 3; i++)
-            {
-                var tempScore = 0;
-                if (i == 1)
-                {
-                    tempScore = player1Points;
-                }
-                else
-                {
-                    score += "-";
-                    tempScore = player2Points;
-                }
-
-                score = GetPlayerGameScore(tempScore, score);
-            }
-
-            return score;
+            return $"{GetPlayerGameScore(_player1Points)}-{GetPlayerGameScore(_player2Points)}";
         }
 
-        private static string GetPlayerGameScore(int tempScore, string score)
+        private bool PointsAreEqual()
         {
-            switch (tempScore)
-            {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
+          return _player1Points == _player2Points;
+        }
 
-            return score;
+        private static string GetPlayerGameScore(int points)
+        {
+          return points switch
+          {
+            0 => "Love",
+            1 => "Fifteen",
+            2 => "Thirty",
+            _ => "Forty"
+          };
         }
 
         private static bool PointGapIsMoreThanOne(int minusResult)
@@ -94,26 +72,11 @@ namespace Tennis
             return minusResult >= 2 ? "Win for player1" : "Win for player2";
         }
 
-        private string GetScoreWhenEqualPoints()
+        private string GetScoreWhenPointsAreEqual()
         {
-            string score;
-            switch (player1Points)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-            }
-
-            return score;
+          return _player1Points < 3 
+            ? $"{GetPlayerGameScore(_player1Points)}-All" 
+            : "Deuce";
         }
     }
 }
